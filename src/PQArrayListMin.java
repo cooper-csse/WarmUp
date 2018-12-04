403270d9acd670671f63e976050b7c6117d97671
Cooper Anderson <cooper@cooperstuff.ninja>
2018-12-04 18:49:20 -0500

    Added Priority Queue functionality

--- src/PQArrayListMin.java
+++ src/PQArrayListMin.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /**
  * An implementation of the Priority Queue interface using an array list.
  * 
- * @author Matt Boutell and <<TODO: Your name here>>>. Created Mar 29, 2014.
+ * @author Matt Boutell and <Cooper Anderson>. Created Mar 29, 2014.
  * 
  * @param <T>
  *            Generic type of PQ elements
@@ -14,35 +14,60 @@ public class PQArrayListMin<T extends Comparable<T>> {
 	private ArrayList<T> items;
 
 	public PQArrayListMin() {
-		// TODO: implement
+		this.items = new ArrayList<T>();
 	}
 
 	public T findMin() {
-		// This is also known as peekMin
-		// TODO: implement
-		return null;
+		return this.isEmpty() ? null : this.items.get(this.items.size()-1);
 	}
 
 	public T deleteMin() {
-		// TODO: implement
-		return null;
+		return this.isEmpty() ? null : this.items.remove(this.items.size()-1);
 	}
 
 	public void insert(T item) {
-		// TODO: implement
+		// This implementation is O(n), as it traverses the whole list:
+		for (int i = 0; i < this.items.size(); i++) {
+			if (item.compareTo(this.items.get(i)) > 0) {
+				this.items.add(i, item);
+				return;
+			}
+		}
+		this.items.add(item);
+
+		// This implementation is O(log(n)), as it uses a binary search to find where to put the new item:
+		/*int length = this.items.size();
+		if (length == 0) {
+			this.items.add(item);
+			return;
+		}
+		int index = Math.floorDiv(length - 1, 2);
+		System.out.println("Initial Index: " + index + " " + Math.ceil(Math.log(length) / Math.log(2)));
+		for (int i = 0; i <= Math.ceil(Math.log(length) / Math.log(2)); i++) {
+			int compare = this.items.get(index).compareTo(item);
+			int direction = -1;
+			if (compare == 0) {
+				break;
+			} else if (compare > 0) {
+				direction = 1;
+			}
+			index += direction * Math.ceil(length / Math.pow(2, i + 2));
+			if (index < 0)
+				index = 0;
+				break;
+		}
+		this.items.add(index, item);*/
 	}
 
 	public int size() {
-		// TODO: implement
-		return -1;
+		return this.items.size();
 	}
 
 	public boolean isEmpty() {
-		// TODO: implement
-		return true;
+		return this.items.isEmpty();
 	}
 
 	public void clear() {
-		// TODO: implement
+		this.items.clear();
 	}
 }
